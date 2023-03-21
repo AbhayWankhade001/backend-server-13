10ca913fd6ddcd07df5b0aadc3b35b1530023bac
AbhayWankhade001 <110882315+AbhayWankhade001@users.noreply.github.com>
2023-03-21 17:47:22 +0530

    Update User.model.js

--- model/User.model.js
+++ model/User.model.js
@@ -136,13 +136,19 @@ export const EmployeeSchema = new Schema({
 
 export const AdminSchema = new Schema({
 
-  email: {
+   email: {
     type: String,
     required: true
   },
   password: {
     type: String,
     required: true
+  },
+  firstName:{
+   type:  String,
+  },
+  lastName:{
+    type : String,
   }
   
 })
